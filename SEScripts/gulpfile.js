23b440cf9ab92f324d7b047296755c30b5f4a4da
Pedro Fonseca <fonseca.pmvasco@gmail.com>
2019-03-24 22:29:05 +0000

    Added gulp task to compile scripts

---
+++ SEScripts/gulpfile.js
@@ -0,0 +1,16 @@
+﻿/// <binding AfterBuild='Compile' />
+/*
+This file is the main entry point for defining Gulp tasks and using Gulp plugins.
+Click here to learn more. https://go.microsoft.com/fwlink/?LinkId=518007
+*/
+
+var gulp = require('gulp');
+var exec = require('child_process').exec;
+
+gulp.task('Compile', function (cb) {
+    exec('ScriptBuilder SEScripts', { cwd: '../' }, function (err, stdout, stderr) {
+        console.log(stdout);
+        console.log(stderr);
+        cb(err);
+    });
+});
